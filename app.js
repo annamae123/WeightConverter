0810f6284901b4bd140504838e0951d40275e543
annamae <annamaebangkok@gmail.com>
2022-10-28 15:27:07 +0800

    First Commit

---
+++ app.js
@@ -0,0 +1,26 @@
+function kilo(){
+    let kilograms = document.getElementById("weightInput").value;
+    const grams = 1000;
+    const ans = grams * kilograms;
+
+   document.getElementById('display').innerHTML = ans;
+   document.getElementById('displayUnit').innerHTML = "Grams";
+}
+function tons(){
+    let kilograms = document.getElementById("weightInput").value;
+    const ton = 0.00098421;
+    const ans = ton * kilograms ;
+
+   document.getElementById('display').innerHTML = ans;
+   document.getElementById('displayUnit').innerHTML = "Tons";
+}
+function Pounds(){
+    let kilograms = document.getElementById("weightInput").value;
+    const pounds = 2.20462262;
+    const ans = kilograms * pounds;
+    // const mins = minute % hour;
+
+//    document.getElementById('display').innerHTML = parseInt (ans) + " Hour/s and " + mins + " minutes";
+document.getElementById('display').innerHTML = ans;
+   document.getElementById('displayUnit').innerHTML = "Pounds";
+}
